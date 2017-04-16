58f6680551421655e37ddfc5da88bc9fd96e04a4
Sarthak Negi <sarthakn18@gmail.com>
2017-04-16 23:25:25 +0530

    Added flac recorder feature

--- GoogleEngine.Designer.cs
+++ GoogleEngine.Designer.cs
@@ -28,7 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GoogleEngine));
+            this.RecordButton = new ns1.BunifuThinButton2();
+            this.SearchButton = new ns1.BunifuThinButton2();
+            this.CountDownLabel = new MetroFramework.Controls.MetroLabel();
             this.SuspendLayout();
+            // 
+            // RecordButton
+            // 
+            this.RecordButton.ActiveBorderThickness = 1;
+            this.RecordButton.ActiveCornerRadius = 20;
+            this.RecordButton.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.RecordButton.ActiveForecolor = System.Drawing.Color.White;
+            this.RecordButton.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.RecordButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.RecordButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("RecordButton.BackgroundImage")));
+            this.RecordButton.ButtonText = "Record";
+            this.RecordButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RecordButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RecordButton.ForeColor = System.Drawing.Color.SeaGreen;
+            this.RecordButton.IdleBorderThickness = 1;
+            this.RecordButton.IdleCornerRadius = 20;
+            this.RecordButton.IdleFillColor = System.Drawing.Color.White;
+            this.RecordButton.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.RecordButton.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.RecordButton.Location = new System.Drawing.Point(44, 231);
+            this.RecordButton.Margin = new System.Windows.Forms.Padding(5);
+            this.RecordButton.Name = "RecordButton";
+            this.RecordButton.Size = new System.Drawing.Size(181, 41);
+            this.RecordButton.TabIndex = 0;
+            this.RecordButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.RecordButton.Click += new System.EventHandler(this.RecordButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.ActiveBorderThickness = 1;
+            this.SearchButton.ActiveCornerRadius = 20;
+            this.SearchButton.ActiveFillColor = System.Drawing.Color.DarkOrange;
+            this.SearchButton.ActiveForecolor = System.Drawing.Color.White;
+            this.SearchButton.ActiveLineColor = System.Drawing.Color.Red;
+            this.SearchButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.SearchButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SearchButton.BackgroundImage")));
+            this.SearchButton.ButtonText = "Search";
+            this.SearchButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SearchButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.ForeColor = System.Drawing.Color.SeaGreen;
+            this.SearchButton.IdleBorderThickness = 1;
+            this.SearchButton.IdleCornerRadius = 20;
+            this.SearchButton.IdleFillColor = System.Drawing.Color.White;
+            this.SearchButton.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.SearchButton.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.SearchButton.Location = new System.Drawing.Point(235, 231);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(5);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(181, 41);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CountDownLabel
+            // 
+            this.CountDownLabel.AutoSize = true;
+            this.CountDownLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.CountDownLabel.Location = new System.Drawing.Point(222, 142);
+            this.CountDownLabel.Name = "CountDownLabel";
+            this.CountDownLabel.Size = new System.Drawing.Size(18, 25);
+            this.CountDownLabel.TabIndex = 2;
+            this.CountDownLabel.Text = "1";
             // 
             // GoogleEngine
             // 
@@ -36,14 +101,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
             this.ClientSize = new System.Drawing.Size(474, 386);
+            this.Controls.Add(this.CountDownLabel);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.RecordButton);
             this.Name = "GoogleEngine";
             this.Text = "Google Engine";
             this.Theme = MetroFramework.MetroThemeStyle.Light;
             this.Load += new System.EventHandler(this.GoogleEngine_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ns1.BunifuThinButton2 RecordButton;
+        private ns1.BunifuThinButton2 SearchButton;
+        private MetroFramework.Controls.MetroLabel CountDownLabel;
     }
 }
--- GoogleEngine.cs
+++ GoogleEngine.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace IgenFinalVersion
 {
@@ -17,9 +18,106 @@ namespace IgenFinalVersion
             InitializeComponent();
         }
 
+        //Instance Variables
+        WaveIn sourceStream = null;
+        WaveFileWriter waveWriter = null;
+
+        System.Timers.Timer aTimer; // Timer to record the wav file
+        System.Timers.Timer countDown; //Timer to play the countdown
+        WaveInCapabilities microphone;
+
+        Boolean wavRecorded = false;
+        
+
         private void GoogleEngine_Load(object sender, EventArgs e)
         {
+            //Find the device
+            microphone = new WaveInCapabilities();
+            Boolean microphoneDetected = true;
 
+            for (int i=0;i<WaveIn.DeviceCount;i++)
+            {
+                if (WaveIn.GetCapabilities(i).ProductName.StartsWith("Microphone"))
+                {
+                    microphoneDetected = true;
+                    Console.WriteLine("Microphone Detected");
+                    microphone = WaveIn.GetCapabilities(i);
+                    break;
+                }
+            }
+
+            if (!microphoneDetected)
+            {
+                MessageBox.Show("No microphone found on your device. Search cannot proceed.");
+                return;
+            }
+        }
+
+        
+        private void RecordButton_Click(object sender, EventArgs e)
+        {
+            string fileName = "C:/Users/PrAnk/Desktop/test.flac";   //Path of the wav file to be recorded into
+
+            sourceStream = new WaveIn();
+            sourceStream.WaveFormat = new WaveFormat(44100,1);
+            sourceStream.DataAvailable += new EventHandler<WaveInEventArgs>(sourceStream_DataAvailable);
+            waveWriter = new WaveFileWriter(fileName,sourceStream.WaveFormat);
+
+            sourceStream.StartRecording();
+
+            aTimer = new System.Timers.Timer();
+            countDown = new System.Timers.Timer();
+
+            countDown.Elapsed += new System.Timers.ElapsedEventHandler(startCountDown);
+            countDown.Interval = 1000;
+            countDown.Enabled = true;
+
+            aTimer.Elapsed += new System.Timers.ElapsedEventHandler(onTimedEvent);
+            aTimer.Interval = 6000;
+            aTimer.Enabled = true;
+
+        }
+
+        int i = 0;
+        private void startCountDown(object sender, EventArgs e)
+        {
+            i++;
+            SetText(i+"");
+            if (i==6)
+            {
+                aTimer.Enabled = false;
+                countDown.Enabled = false;
+                wavRecorded = true;
+            }
+        }
+
+        //To make thread safe call to Window Forms Control
+        delegate void SetTextCallback(String text);
+
+        private void SetText (string text)
+        {
+            if (this.CountDownLabel.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+                this.CountDownLabel.Text = text;
+        }
+
+        private void onTimedEvent(object sender,EventArgs e)
+        {
+            sourceStream.StopRecording();
+            sourceStream = null;
+            MessageBox.Show("recorded");
+        }
+        private void sourceStream_DataAvailable(object sender,WaveInEventArgs e)
+        {
+            if (waveWriter == null)
+                return;
+
+            waveWriter.Write(e.Buffer,0,e.BytesRecorded);
+            waveWriter.Flush();
         }
     }
 }
